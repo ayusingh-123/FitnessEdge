968323b7853b00cfc6e69c95121844a875be3320
Ayush Singh Parihar <ayushsinghparihar7@gmail.com>
2022-11-10 15:57:02 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BmiComponent } from './bmi/bmi.component';
+import { BodyComponent } from './body/body.component';
 
 
 const routes: Routes = [
+  { path : 'body', component: BodyComponent},
   { path: 'bmi', component: BmiComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
 
 
 
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    BodyComponent,
   ],
   imports: [
     BrowserModule,
